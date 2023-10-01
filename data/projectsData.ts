c725f2668b39c286d40d2d6cfaa396eabbb3ae89
Jun Sakai <sakaijunsoccer@gmail.com>
2023-09-30 22:04:33 -1000

    Changed default metadata

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,23 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: '次世代AIを活用した教育サイトAlamoa',
+    description: `世代AIを活用した教育サイトAlamoaを開設しました。
+    同サイトでは、、AIが学習者の回答を24時間行うことにより学習者の疑問をすぐに解決しますので、より効果的な学習が可能となります。 
+    同サイトでは、PythonやJavaなどのプログラミング言語から、機械学習やデータサイエンスなどの応用分野まで、
+    幅広いコースが用意されています。また、初心者向けのコースから上級者向けのコースまで、
+    学習者のレベルに合わせたカリキュラムが提供されています。
+    alamoaの次世代AIを活用した教育サイトで、効率的かつ効果的な学習を始めましょう`,
+    imgSrc: '/static/images/alamoa.png',
+    href: 'http://www.alamoa.io',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'インフルエンサーインプレッション向上のための質問回答サービスQD pocket',
+    description: `このサービスでは、インフルエンサーが自分のプロフィールページを作成し、
+    フォロワーからの質問に答えることができます。
+    質問は匿名で送信されるため、フォロワーが気軽に質問を投稿できるという特徴があります。
+    また、インフルエンサーは自分のプロフィールページに宣伝文章を掲載することもできます。`,
+    imgSrc: '/static/images/qdpocket.png',
+    href: 'http://q.alamoa.io',
   },
 ]
 
